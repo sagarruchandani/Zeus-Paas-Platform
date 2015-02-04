a59c8e671cdf6d67eb0b0356071d3f1d1e540323
Sagar Ruchandani <sagar.ruchandani@sjsu.edu>
2015-02-03 17:34:55 -0800

    removed aws key

--- routes/createdomain.js
+++ routes/createdomain.js
@@ -1,8 +1,8 @@
 var Route53 = require('nice-route53');
 
 var r53 = new Route53({
-accessKeyId :  "AKIAJQP7U5B7N2RG5MZA",
-secretAccessKey :"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" 
+accessKeyId :  "AWS_ACCESS_KEY",
+secretAccessKey :"AWS_SECRET_KEY" 
 });
 
 module.exports = {
